c7d2753dcd26e67ddfaeabc4874141da2b9bcd3d
WEMABANK\Adeshina.falade <adeshina.falade@wemabank.com>
2023-08-01 23:37:43 +0100

    done

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,7 +1,7 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,37 +9,44 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import dev.icerock.moko.mvvm.compose.ViewModelFactory
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import model.BirdImage
 
 @Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+){
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdsViewModel = getViewModel(Unit, viewModelFactory{ BirdsViewModel() })
         BirdsPage(birdsViewModel)
     }
@@ -50,15 +57,39 @@ fun App() {
 fun BirdsPage(viewModel: BirdsViewModel){
 
     val uiState by viewModel.uiState.collectAsState()
-    AnimatedVisibility(uiState.images.isNotEmpty()){
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(5.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp),
-            modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            Modifier.fillMaxWidth().padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
         ){
-            items(uiState.images){
-                BirdImageCell(it)
+            for (category in uiState.categories){
+                Button(
+                    onClick = {viewModel.selectCategory(category)},
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    )
+                ){
+                    Text(text = category)
+                }
+            }
+        }
+        AnimatedVisibility(uiState.selectedImages.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp)
+            ) {
+                items(uiState.selectedImages) {
+                    BirdImageCell(it)
+                }
             }
         }
     }
--- shared/src/commonMain/kotlin/BirdsViewModel.kt
+++ shared/src/commonMain/kotlin/BirdsViewModel.kt
@@ -20,8 +20,12 @@ import kotlinx.serialization.json.jsonPrimitive
 import model.BirdImage
 
 data class BirdsUiState(
-    val images: List<BirdImage> = emptyList()
-)
+    val images: List<BirdImage> = emptyList(),
+    val selectedCategory: String? = null
+){
+    val categories = images.map { it.category }.toSet()
+    val selectedImages = images.filter { it.category == selectedCategory }
+}
 
 class BirdsViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(BirdsUiState())
@@ -45,6 +49,12 @@ class BirdsViewModel: ViewModel() {
 
     override fun onCleared() {
         httpClient.close()
+    }
+
+    fun selectCategory(category: String){
+        _uiState.update {
+            it.copy(selectedCategory = category)
+        }
     }
 
     fun updateImages(){
